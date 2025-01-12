0c493d908e2348fd5ad96dbbcc11f545464c2b18
thuyen.vunguyen <audynamite234@gmail.com>
2025-01-12 17:59:39 +0700

    implement with JSX

--- src/components/learn/SecondComponent.jsx
+++ src/components/learn/SecondComponent.jsx
@@ -1,6 +1,18 @@
+import './style.css'
 const SecondComponent = () => {
+    const varstr = {
+        name: "ThuyenVN",
+        age: "30"
+    };
+    const num = [1, 2, 3];
     return (
-        <div> Second Component</div>
+        <>
+            <div className='child'
+                style={{ borderRadius: "10px" }}
+            > test 2 {JSON.stringify(varstr)} {num[0]}</div>
+            <div></div>
+
+        </>
     );
 }
 
