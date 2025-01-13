a605bd253c01aeaa26cc9630a3c643d61a60a63d
thuyen.vunguyen <audynamite234@gmail.com>
2025-01-14 00:13:59 +0700

    props

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,7 +1,11 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    console.log(">> check props:", props)
+    const { name, age, data } = props
     return (
         <div className="todo-data">
-            <div> learning react</div>
+            <div>My name is: {name}</div>
+            <div>my country: {props.data.country}</div>
+            <div> Learning react</div>
         </div>
     )
 }
